c2ddba600724465ea69e9adad5d397e3888eb923
HelyLi <1772063751@qq.com>
2020-02-27 10:47:46 +0800

    add py

---
+++ main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import os
+import eyed3
+
+
+class Mp3Genre(object):
+    """
+    方便自己修改mp3文件的流派
+    2014-02-14
+    """
+    
+    def __init__(self, path):
+        self.path = "/Users/triangle/res_o/audio"
+        self.mp3_list = []
+        self.fun()
+    
+    def fun(self):
+        """
+        将查找到的mp3添加到列表
+        """
+        for root, dirs, files in os.walk("/Users/triangle/res_o/audio/effect/ios/com/play"):
+            dirs[:] = []
+            for fn in files:
+                mp3_path = root + "/" + fn
+                ext = os.path.splitext(mp3_path)[1]
+                if ext in (".MP3", ".mp3"):
+                    self.mp3_list.append(mp3_path)
+                    
+    def todo(self, genre):
+        """
+        导入eyed3修改mp3流派
+        """
+        if self.mp3_list:
+            for mp3_path in self.mp3_list:
+                print(mp3_path)
+                load_file = eyed3.load(mp3_path)
+                load_file.tag._setGenre(genre)
+                load_file.tag.save(encoding="utf8")
+            print("处理完毕")
+        else:
+            print("找不到mp3后缀的歌曲")
+ 
+ 
+if __name__ == "__main__":
+    mp3_genre = Mp3Genre(r"/Users/triangle/res_o/audio")
+    mp3_genre.todo(u"摇滚")
